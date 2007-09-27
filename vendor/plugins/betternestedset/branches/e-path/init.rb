69057a807565b36b23ee0e9605f8dd98af7d5dd7
qm <qm@5907ca08-143b-0410-a311-b8a4f5c60375>
2007-09-27 17:24:01 +0000

    removed betternestedset
    git-svn-id: svn+ssh://youreasyhome.com/home/qm/svn/tcm/trunk@7 5907ca08-143b-0410-a311-b8a4f5c60375

--- vendor/plugins/betternestedset/branches/e-path/init.rb
+++
@@ -1,9 +0,0 @@
-# better_nested_set
-# (c) 2005 Jean-Christophe Michel 
-# MIT licence
-#
-require 'better_nested_set'
-
-ActiveRecord::Base.class_eval do
-  include FooCom::Acts::EnumeratedPath
-end
