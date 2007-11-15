fe96d1198fe80bfa446099c0d6d7964ba8bcb2ea
qm <qm@5907ca08-143b-0410-a311-b8a4f5c60375>
2007-11-15 06:11:52 +0000

    fixed searching without an option
    git-svn-id: svn+ssh://youreasyhome.com/home/qm/svn/tcm/trunk@114 5907ca08-143b-0410-a311-b8a4f5c60375

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ config.cache_classes = true
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
 # config.logger = Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}.log", 10, 10000000)
-config.log_level = :debug
+# config.log_level = :debug
 
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
@@ -19,8 +19,8 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 require 'memcache'
-CACHE = MemCache.new 'fisharc.local:11211', :namespace => 'koala_track'
+CACHE = MemCache.new '127.0.0.1:11211', :namespace => 'koala_track'
 
-# config.action_controller.session_store = :mem_cache_store
+config.action_controller.session_store = :mem_cache_store
 # config.action_controller.session_store = :active_record_store
 # config.action_controller.fragment_cache_store = :mem_cache_store
