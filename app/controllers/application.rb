4a5479c96e6f0f448f7ce820812ffee4f98180c0
qm <qm@5907ca08-143b-0410-a311-b8a4f5c60375>
2008-03-23 23:36:06 +0000

    SSL
    git-svn-id: svn+ssh://youreasyhome.com/home/qm/svn/tcm/trunk@182 5907ca08-143b-0410-a311-b8a4f5c60375

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
-  #include SslRequirement
+  include SslRequirement
   # If you want "remember me" functionality, add this before_filter to Application Controller
   # before_filter :login_from_cookie  
 end
