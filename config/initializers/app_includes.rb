af32728cc26b8c1586b921a32cdcea8e803c1bf0
qm <qm@5907ca08-143b-0410-a311-b8a4f5c60375>
2008-07-28 02:09:35 +0000

    upgrading
    git-svn-id: svn+ssh://youreasyhome.com/home/qm/svn/tcm/trunk@211 5907ca08-143b-0410-a311-b8a4f5c60375

--- config/initializers/app_includes.rb
+++ config/initializers/app_includes.rb
@@ -4,7 +4,11 @@ require 'acts_as_solr'
 module ActsAsSolr 
   module ClassMethods 
     def paginate_search(query, options = {}, find_options = {}) 
-      options, page, per_page = wp_parse_options!(options) 
+      page, per_page, total = wp_parse_options(options) 
+      options.delete(:per_page)
+      options.delete(:page)
+      
+      logger.info ("options:#{options.inspect} page:#{page.inspect} per_page:#{per_page.inspect}")
       pager = WillPaginate::Collection.new(page, per_page, nil) 
       options.merge!(:offset => pager.offset, :limit => per_page)
       result = find_by_solr(query, options, find_options) 
