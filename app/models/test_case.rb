00d23ba6f0797afe07a85904bf7e815f6b6e03e2
Andrew Coldham <andrewcoldham@mac.com>
2009-07-16 15:01:07 -0700

    hackity

--- app/models/test_case.rb
+++ app/models/test_case.rb
@@ -24,7 +24,7 @@
 class TestCase < ActiveRecord::Base
   acts_as_taggable
   
-  set_cached_tag_list_column_name "tag"
+  #set_cached_tag_list_column_name "tag"
   
   acts_as_versioned 
   def owner
